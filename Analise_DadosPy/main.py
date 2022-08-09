a72fcc69d9beb3f16e2f9d641bceb1ad7de78712
Felipe Bertoli <felipebertolioliveira@gmail.com>
2022-08-09 15:11:44 -0300

    Segundo Commit

--- Analise_DadosPy/main.py
+++ Analise_DadosPy/main.py
@@ -1,23 +1,14 @@
 import pandas as pd
-import plotly
+import plotly.express as px
 
 table = pd.read_csv("users.csv")
 
 # 1. Visualizar a base de dados
 table = table.drop("Unnamed: 0", axis=1)
 
-# 2. Tratamento de erros
-# - Valores que estão reconhecidos de forma errada
-table["TotalGasto"] = pd.to_numeric(table["TotalGasto"], errors="coerce")
-print(table)
-
-# - Valores vazios
-# deletando as colunas vazias
-table = table.dropna(how="all", axis=1)
-# deletando as linhas vazias
-table = table.dropna(how="any", axis=0)
-
-# 3. Análise dos cancelamentos
-print(table["Churn"].value_counts(normalize=True).map("{:.1%}".format))
-
-# 4. Análise dos motivos do cancelamento
+# 2. Análise dos motivos do cancelamento
+for column in table.columns:
+        i = int(i + 1)
+        graph = px.histogram(table, x=column, color="Churn", color_discrete_sequence=["red", "green"], text_auto=True)
+        graph.write_image("output/graph" + str(i) + ".png") 
+        print("-> GRÁFICO GERADO!")
